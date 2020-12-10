8de8672594d8744e7dfe7da77f98c16893a09576
kaykobad <kaykobadreza@gmail.com>
2020-12-10 10:24:13 +0600

    Add database utility class

---
+++ lib/data/database/db_utils.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:prizebond_manager/data/models/prizebond.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DBProvider {
+  DBProvider._();
+  static final DBProvider db = DBProvider._();
+
+  Database _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database;
+    _database = await initDB();
+    return _database;
+  }
+
+  initDB() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, "prizeBondManager.db");
+    return await openDatabase(path, version: 1,
+      onOpen: (db) {},
+      onCreate: (Database db, int version) async {
+        await db.execute("CREATE TABLE PrizeBond ("
+            "id INTEGER PRIMARY KEY,"
+            "prizeBondNumber TEXT,"
+            "insertDate TEXT,"
+            "updateDate TEXT"
+            ")"
+        );
+      },
+    );
+  }
+
+  Future<int> insertPrizeBond(PrizeBond prizeBond) async {
+    final db = await database;
+    return await db.insert("PrizeBond", prizeBond.toMap());
+  }
+
+  Future<int> updatePrizeBond(PrizeBond prizeBond) async {
+    final db = await database;
+    return await db.update("PrizeBond", prizeBond.toMap(), where: "id = ?", whereArgs: [prizeBond.id]);
+  }
+
+  Future<int> deletePrizeBondById(int id) async {
+    final db = await database;
+    return await db.delete("PrizeBond", where: "id = ?", whereArgs: [id]);
+  }
+
+  Future<List<PrizeBond>> getAllPrizeBonds() async {
+    final db = await database;
+    var res = await db.query("PrizeBond");
+    List<PrizeBond> list = res.isNotEmpty ? res.map((c) => PrizeBond.fromMap(c)).toList() : [];
+    return list;
+  }
+}
--- lib/data/models/prizebond.dart
+++ lib/data/models/prizebond.dart
@@ -1,10 +1,17 @@
 class PrizeBond {
   final int id;
   final String prizeBondNumber;
-  final DateTime insertDate;
-  final DateTime updateDate;
+  final String insertDate;
+  final String updateDate;
 
   PrizeBond({this.id, this.prizeBondNumber, this.insertDate, this.updateDate});
+
+  factory PrizeBond.fromMap(Map<String, dynamic> json) => PrizeBond(
+    id: json["id"],
+    prizeBondNumber: json["prizeBondNumber"],
+    insertDate: json["insertDate"],
+    updateDate: json["updateDate"],
+  );
 
   Map<String, dynamic> toMap() {
     return {
@@ -14,4 +21,7 @@ class PrizeBond {
       'updateDate': updateDate,
     };
   }
+
+  @override
+  String toString() => "PrizeBond {id: $id, prizeBondNumber: $prizeBondNumber, insertDate: $insertDate, updateDate: $updateDate}";
 }
