f536c5048754330f3cae4eb1f157233caacb942e
kaykobad <kaykobadreza@gmail.com>
2020-12-10 11:22:35 +0600

    Show alert dialog on database error

--- lib/constants/string_constants.dart
+++ lib/constants/string_constants.dart
@@ -1,1 +1,5 @@
 const String APP_NAME = "Prizebond Manager";
+const String DATABASE_READ_ERROR = "Database Error";
+const String DATABASE_READ_ERROR_DETAILS = "An unexpected error occurred while reading database.";
+const String BUTTON_RETRY = "Retry";
+const String BUTTON_CLOSE_APPLICATION = "Exit";
--- lib/ui/splash_screen/splash_screen.dart
+++ lib/ui/splash_screen/splash_screen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:prizebond_manager/constants/number_constants.dart';
 import 'package:prizebond_manager/constants/string_constants.dart';
+import 'package:prizebond_manager/data/data_store/data_store.dart';
+import 'splash_screen_bloc.dart';
+import 'splash_screen_event.dart';
+import 'splash_screen_state.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -9,30 +15,81 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  DataStore _dataStore;
+  SplashScreenBloc _splashScreenBloc;
+  
+  @override
+  void initState() {
+    super.initState();
+    _dataStore = RepositoryProvider.of<DataStore>(context);
+    _splashScreenBloc = SplashScreenBloc();
+    _splashScreenBloc.add(InitiateDataFetchEvent());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple,
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              APP_NAME,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: SPLASH_APP_NAME_FONT_SIZE,
-                fontWeight: FontWeight.w600,
+      body: BlocListener(
+        cubit: _splashScreenBloc,
+        listener: (context, state) {
+          if (state is DataFetchSuccessState) {
+            _dataStore.allPrizeBonds = state.allPrizeBonds;
+            // TODO: Navigate to home page
+            print(state.allPrizeBonds);
+          } else if (state is DataFetchFailureState) {
+            _showErrorDialog();
+          }
+        },
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                APP_NAME,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: SPLASH_APP_NAME_FONT_SIZE,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
-            ),
-            SizedBox(height: 20.0),
-            SpinKitThreeBounce(
-              color: Colors.white,
-              size: 20.0,
-            ),
-          ],
+              SizedBox(height: 20.0),
+              SpinKitThreeBounce(
+                color: Colors.white,
+                size: 20.0,
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  _showErrorDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(DATABASE_READ_ERROR),
+          content: Text(DATABASE_READ_ERROR_DETAILS),
+          actions: [
+            FlatButton(
+              child: Text(
+                BUTTON_CLOSE_APPLICATION,
+                style: TextStyle(color: Colors.redAccent),
+              ),
+              onPressed: () => SystemNavigator.pop(),
+            ),
+            FlatButton(
+              child: Text(BUTTON_RETRY),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _splashScreenBloc.add(InitiateDataFetchEvent());
+              },
+            ),
+          ],
+        );
+      }
     );
   }
 }
